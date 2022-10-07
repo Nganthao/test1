3bc09ae9c78c5bcaa179bd032df29315844b4b8a
Thao Ngan <tltngan1900438@student.ctuet.edu.vn>
2022-10-07 20:41:28 +0700

    create test 1

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace NetMVC.Controllers
             return View();
         }
 
+         public IActionResult Planet()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
